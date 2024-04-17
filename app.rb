e9a01b844f5206b0735913d060bab2c1edfdfb8f
ianhaubold <165107627+ianhaubold@users.noreply.github.com>
2024-04-17 02:21:46 +0000

    payment

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 
-
+## SQUARE
 get("/square/new") do
   erb(:new_square_calc)
 end
@@ -11,6 +11,34 @@ get '/square/results' do
 
   @the_result = @the_num**2
   erb(:square_results)
+end
+
+## SQUARE ROOT
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get '/square_root/results' do
+  @the_num2 = params.fetch("the_num2").to_f
+
+  @the_result2 = @the_num2**0.5
+  erb(:square_root_results)
+end
+
+## PAYMENT
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get '/payment/results' do
+  @apr = params.fetch("user_apr").to_f/100/12
+  @periods = params.fetch("years").to_i*12
+  @principal = params.fetch("principal)").to_i
+  @numerator = @principal * @apr
+  @denominator = (1-(1+@apr)**-@periods)
+
+  @the_result3 = @numerator / @denominator
+  erb(:payment_results)
 end
 
 get("/") do
