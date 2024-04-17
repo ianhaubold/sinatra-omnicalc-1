dae18ac7e516bc9ba278622a4a656f72c8935720
ianhaubold <165107627+ianhaubold@users.noreply.github.com>
2024-04-17 03:15:50 +0000

    random

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ get("/square_root/new") do
 end
 
 get '/square_root/results' do
-  @the_num2 = params.fetch("the_num2").to_f
+  @the_num2 = params.fetch("users_number").to_f
 
   @the_result2 = @the_num2**0.5
   erb(:square_root_results)
@@ -30,15 +30,29 @@ get("/payment/new") do
   erb(:new_payment_calc)
 end
 
-get '/payment/results' do
-  @apr = params.fetch("user_apr").to_f/100/12
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @monthly_rate = @apr/12/100
   @periods = params.fetch("years").to_i*12
-  @principal = params.fetch("principal)").to_i
-  @numerator = @principal * @apr
-  @denominator = (1-(1+@apr)**-@periods)
+  @principal = params.fetch("principal").to_i
+  @numerator = @principal * @monthly_rate
+  @denominator = (1-(1+@monthly_rate)**-@periods)
 
   @the_result3 = @numerator / @denominator
   erb(:payment_results)
+end
+
+## Random
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @the_result4 = rand(@min..@max)
+
+  erb(:random_results)
 end
 
 get("/") do
